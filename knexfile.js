b109f2c1377c14bb2d0a5f92ab713a5feb4798d9
Caroline O'Donnell <caroline.odonnell26@gmail.com>
2017-09-12 13:57:56 -0600

    made separate routes for get nurses and patients, singular and all

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,16 @@
 // Update with your config settings.
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 
-//dotenv.config();
+dotenv.config();
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/chargenurse'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL  + '?ssl=true'
   }
-  ,
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL  + '?ssl=true'
-  // }
 };
